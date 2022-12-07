f69f21b361d91f707da0820ff086306639b13141
hamadatakaki <i16hamadat@gmail.com>
2022-12-08 03:26:55 +0900

    update: fetcher.v

---
+++ cpuv1/cpuv1.srcs/sources_1/new/fetcher.v
@@ -0,0 +1,53 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2022/12/08 02:56:14
+// Design Name: 
+// Module Name: fetcher
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+// without Program-Counter
+
+module fetcher_v1(
+    input wire [31:0] pc,
+    output wire [31:0] instruction
+);
+    reg [31:0] mem [0:31];
+
+    assign instruction = mem[pc];
+endmodule
+
+// with Program-Counter increment, overwrite
+
+module fetcher_v2(
+    input wire clk_fetch,
+    input wire write_pc,
+    input wire [31:0] pc_new,
+    output wire [31:0] instruction
+);
+    reg [31:0] mem [0:31];
+    reg [31:0] pc = 0;
+
+    assign instruction = mem[pc];
+
+    always @(posedge clk_fetch) begin
+        pc <= pc + 1;
+        
+        if (write_pc == 1) begin
+            pc <= pc_new;
+        end
+    end
+endmodule

5ae562b4d032006ea1148a080bba78081d5ee25d
hamadatakaki <i16hamadat@gmail.com>
2022-12-12 03:45:09 +0900

    update: register file test write

--- cpuv1/cpuv1.srcs/sim_1/new/sim_register_file.v
+++ cpuv1/cpuv1.srcs/sim_1/new/sim_register_file.v
@@ -20,7 +20,13 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 `define CYCLE_NS 10
-`define N_CYCLES 5
+`define N_CYCLES 15
+
+`define test_write_register(t_wait, a, d) \
+    write_regfile <= 1; \
+    addr_write <= a; \
+    data_write <= d; \
+    #(`CYCLE_NS * t_wait)
 
 module sim_register_file;
 
@@ -47,15 +53,21 @@ module sim_register_file;
         .rd(rd)
     );
     
+    reg write_regfile = 0;
     wire [31:0] data_rs1, data_rs2;
+
+    reg [4:0] addr_write = 0;
+    reg [31:0] data_write;
 
     register_file_v3 register_file0 (
         .clk(clk),
-        .write(write),
+        .write(write_regfile),
         .addr_rs1(rs1),
         .addr_rs2(rs2),
         .data_rs1(data_rs1),
-        .data_rs2(data_rs2)
+        .data_rs2(data_rs2),
+        .addr_write(addr_write),
+        .data_write(data_write)
     );
 
     always #(`CYCLE_NS / 2) begin
@@ -71,5 +83,12 @@ module sim_register_file;
         $readmemh("./instructions.hex", fetcher0.rom0.mem);
         
         #(`CYCLE_NS * `N_CYCLES);
+        
+        `test_write_register(4, 1, 1);
+        `test_write_register(4, 2, 2);
+        `test_write_register(4, 3, 4);
+        `test_write_register(4, 4, 8);
+        
+        $finish;
     end
 endmodule
